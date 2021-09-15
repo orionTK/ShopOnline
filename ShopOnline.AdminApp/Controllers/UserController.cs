5f7c200a5558406f824fdeb4dacec49a7a365b59
OrionTK <saobangxanh24@gmail.com>
2021-09-15 22:14:24 +0700

    add user api

---
+++ ShopOnline.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.ViewModel.Users.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnline.AdminApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginRequest rq)
+        {
+            return View();
+        }
+    }
+}
