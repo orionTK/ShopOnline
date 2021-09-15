2a3cd6b41e6115f45ca2013f3d4bd5b09cc9f4b6
OrionTK <saobangxanh24@gmail.com>
2021-09-15 12:07:12 +0700

    update login and register

--- ShopOnline.ViewModel/System/Users/RegisterRequest.cs
+++ ShopOnline.ViewModel/System/Users/RegisterRequest.cs
@@ -13,6 +13,7 @@ namespace ShopOnline.ViewModel.Users.System
         public string PhoneNumber { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        //123456TK@vt
         public string ConfirmPassword { get; set; }
 
     }
